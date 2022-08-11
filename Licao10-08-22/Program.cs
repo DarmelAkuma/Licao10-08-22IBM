a4997b61b4bb9ed68338484cc4c36f76aaa9f3c9
Mateus Melo Oliveira <Mateus99Melo@gmail.com>
2022-08-11 10:59:15 -0300

    Inicio do Projeto

---
+++ Licao10-08-22/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Licao10_08_22 {
+    class Program {
+        static void Main(string[] args) {
+            Tabuada();
+        }
+        static void Tabuada() {
+            int numero, resultado;
+            Console.WriteLine("Digite o número: ");
+            numero = int.Parse(Console.ReadLine());
+            for (int x = 0; x <= 10; x++) {
+                resultado = numero * x;
+                Console.WriteLine(x + " X " + numero + " = " + resultado);
+            }
+        }
+    }
+}
